b451d90f421fcc0dd409ca07abf70698de15de1c
olikbolik <viluraor@gmail.com>
2019-05-14 20:17:10 +0300

    Created CreditCard

---
+++ CreditCard/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CreditCard
+{
+    public class CreditCard
+    {
+        private decimal balance = 0;
+        private String number;
+        public string GetMoney()
+        {
+            return "Balance: " + balance.ToString();
+        }
+        public void fullfill(decimal amount)
+        {
+            this.balance += amount;
+        }
+        public string GetNumber()
+        {
+            return this.number;
+        }
+        public CreditCard(String number)
+        {
+            this.number = number;
+        }
+    }
+}
