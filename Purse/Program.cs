21d0de9167ce4b493b8bbbe0b58149242764ec04
olikbolik <viluraor@gmail.com>
2019-05-14 19:47:48 +0300

    Created solution

---
+++ Purse/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Purse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var money = new Money.Money(100);
+            Console.WriteLine(money.GetMoney());    
+        }
+    }
+}
---
+++ money/Money.cs
@@ -0,0 +1,16 @@
+﻿namespace Money
+{
+    public class Money
+    {
+        private decimal amount = 0;
+        private string msg = "Money...pshhhh";
+        public string GetMoney()
+        {
+            return msg + amount.ToString();
+        }
+        public Money(decimal amount)
+        {
+            this.amount = amount;
+        }
+    }
+}
